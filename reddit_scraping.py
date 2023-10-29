3e65912f624ac79b2dbbe2f85babd8f9cc163c20
JiaqiFlora <jiaqigan.kiki@gmail.com>
2023-10-29 15:21:56 -0400

    scrape reddit hottest 1000

--- reddit_scraping.py
+++ reddit_scraping.py
@@ -4,32 +4,40 @@ import csv
 post_num = 1000
 
 # Reddit API credentials
-CLIENT_ID = "YOUR_CLIENT_ID"
-CLIENT_SECRET = "YOUR_CLIENT_SECRET"
-USER_AGENT = "YOUR_USER_AGENT"  # e.g., "my_app/0.0.1"
+CLIENT_ID = "H-4dRHrbjYWOUf0Pt4HntA"
+CLIENT_SECRET = "fO9tlv7PAIsgVGw_K4XB58b402mKzw"
+USER_AGENT = True  # e.g., "my_app/0.0.1"
 
 # Initialize Reddit API client
-reddit = praw.Reddit(
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    user_agent=USER_AGENT
-)
+# reddit = praw.Reddit(
+#     client_id=CLIENT_ID,
+#     client_secret=CLIENT_SECRET,
+#     user_agent=USER_AGENT
+# )
+
+reddit = praw.Reddit(user_agent=True, client_id="H-4dRHrbjYWOUf0Pt4HntA",
+  client_secret="fO9tlv7PAIsgVGw_K4XB58b402mKzw", username='CJ-Darty', password='Darty4Life')
+
 
 # Get hot posts
-hot_posts = reddit.front.hot(limit=post_num)  # Adjust limit as necessary
-
+hot_posts = reddit.subreddit("all").hot(limit=post_num)
 data = []
 
+
 for post in hot_posts:
+    print("\n\n===========a new post!=========")
+    print(post.title)
+    print(post.num_comments)
+
     if post.num_comments > 0:
         post.comment_sort = 'best'  # Sorting comments by 'best'
-        post.comment_limit = 1  # Limiting to 1 comment
+        # post.comment_limit = 1  # Limiting to 1 comment
 
-        print(post.comments)
-        print(len(post.comments))
+        print("###################")
+        print(post.title)
+        print(post.comments[0].body)
+        data.append([post.title, post.comments[0].body])
 
-        for top_comment in post.comments:
-            data.append([post.title, top_comment.body])
 
 # Write to CSV
 with open(f"dataset/reddit/reddit_data_{post_num}.csv", "w", newline="") as csvfile:
